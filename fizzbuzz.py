3e21769027cfcf2f6120cd858cb2b000af99a47c
Aytijha <ac.aytijha@gmail.com>
2021-07-23 14:03:56 +0530

    fizzbuzz game

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+for fizzbuzz in range(100):
+    if fizzbuzz % 15 == 0:
+        print('FizzBuzz')
+    elif fizzbuzz % 3 == 0:
+        print('Fizz')
+    elif fizzbuzz % 5 == 0:
+        print('Buzz')
+    else:
+        print(fizzbuzz)
