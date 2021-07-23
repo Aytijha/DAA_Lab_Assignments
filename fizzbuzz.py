52b76fa20c2f160791e04aac01d96267f4b674d9
Aytijha <ac.aytijha@gmail.com>
2021-07-23 20:55:06 +0530

    FizzBuzz optimized solution

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-for fizzbuzz in range(100):
+'''for fizzbuzz in range(1, 100):
     if fizzbuzz % 15 == 0:
         print('FizzBuzz')
     elif fizzbuzz % 3 == 0:
@@ -6,4 +6,22 @@ for fizzbuzz in range(100):
     elif fizzbuzz % 5 == 0:
         print('Buzz')
     else:
+        print(fizzbuzz)'''
+
+#alternative approach
+c3 = c5 = 0
+for fizzbuzz in range(1, 100):
+    c3+=1
+    c5+=1
+    s = ""
+    if c3 == 3:
+        s += "Fizz"
+        c3 = 0
+    if c5 == 5:
+        s += "Buzz"
+        c5 = 0
+    
+    if s == "":
         print(fizzbuzz)
+    else:
+        print(s)
